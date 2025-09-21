ba4650c964ebe1f8ac3594131b76a78d0e721524
programming-teacher <adi.programming.data@gmail.com>
2025-09-21 21:17:06 +0300

    refactor: add toJSON transformation settings to mongoose configuration

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -9,6 +9,15 @@ async function connectDB() {
     try {
         // silence Mongoose deprecation about strictQuery changing in v7
         mongoose.set("strictQuery", false);
+        mongoose.set('toJSON', {
+            virtuals: true,
+            transform: (doc, converted) => {
+                delete converted._id;
+                delete converted.__v;
+                delete converted.password;
+            }
+        });
+
         await mongoose.connect(MONGO_URI);
         console.log("MongoDB connected");
     } catch (err) {
